958672605af8af29691325fa473c70cf6ccdea47
王智源 <wangzhiyuan@sensorsdata.cn>
2023-05-31 10:46:54 +0800

    update

--- server/src/common/error.rs
+++ server/src/common/error.rs
@@ -8,7 +8,7 @@ pub enum Error {
     #[error("invalid argument, {0}")]
     InvalidArgument(String),
 
-    #[error("not found, code {0}, message {1}")]
+    #[error("not found, code: {0}, message: {1}")]
     NotFound(NotFoundCode, String),
 
     #[error("internal error, {0}")]
